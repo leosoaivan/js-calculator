199e257096018b96ef55cedcc13945e0dae683e2
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-02-06 14:32:45 -0500

    Restructure app and config gulp for sass

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+})
